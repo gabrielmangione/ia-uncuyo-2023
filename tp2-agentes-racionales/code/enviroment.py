49bae82460ef5a8169216fb4897260a37da00628
gabrielmangione <89273551+gabrielmangione@users.noreply.github.com>
2023-09-15 22:06:19 -0300

    Tp2

--- tp2-agentes-racionales/code/enviroment.py
+++ tp2-agentes-racionales/code/enviroment.py
@@ -1,44 +1,7 @@
-#B) Implementar un simulador que determine la medida de rendimiento para el entorno del mundo de la aspiradora según las siguientes especificaciones:
+#Implementar un simulador que determine la medida de rendimiento para el entorno del mundo de la aspiradora según las siguientes especificaciones:
 # La medida de rendimiento premia con un punto al agente por cada recuadro que limpia (aspira) en un período de tiempo concreto,
 # a lo largo de una «vida» de 1000 acciones. La «dimensión» de la grilla se conoce a priori pero la distribución de la suciedad y
 # la localización inicial del agente no se conocen (aleatorio). Las cuadrículas se mantienen limpias y aspirando se limpia la cuadrícula
 #en que se encuentra el agente Las acciones Izquierda, Derecha, Arriba, Abajo mueven al agente en dichas direcciones, excepto en el caso
 #en que lo pueda llevar fuera de la grilla. Las acciones permitidas son: Arriba Abajo Izquierda Derecha Limpiar (aspirar) NoHacerNada
 #El agente percibe su locación y si esta contiene suciedad
-
-import random
-
-class Environment:
-    def __init__(self, sizeX, sizeY, init_posX, init_posY, dirt_rate):
-        self.sizeX = sizeX
-        self.sizeY = sizeY
-        self.agent_posX = init_posX
-        self.agent_posY = init_posY
-        self.dirt_rate = dirt_rate
-        self.grid = [[random.random() < dirt_rate for _ in range(sizeY)] for _ in range(sizeX)]
-
-    def accept_action(self, action):
-        if action == 'Izquierda' and self.agent_posY > 0:
-            self.agent_posY -= 1
-        elif action == 'Derecha' and self.agent_posY < self.sizeY - 1:
-            self.agent_posY += 1
-        elif action == 'Arriba' and self.agent_posX > 0:
-            self.agent_posX -= 1
-        elif action == 'Abajo' and self.agent_posX < self.sizeX - 1:
-            self.agent_posX += 1
-        elif action == 'Limpiar':
-            self.clean()
-        
-    def is_dirty(self):
-        return self.grid[self.agent_posX][self.agent_posY]
-
-    def clean(self):
-        if self.is_dirty():
-            self.grid[self.agent_posX][self.agent_posY] = False
-
-    def get_performance(self):
-        return sum(row.count(False) for row in self.grid)  # Count clean cells
-
-    def print_environment(self):
-        for row in self.grid:
-            print(' '.join(['D' if cell else 'L' for cell in row]))  # D for dirty, L for clean
--- tp2-agentes-racionales/code/main.py
+++ tp2-agentes-racionales/code/main.py
@@ -1,58 +1,6 @@
-import agent,enviroment,random,time
-
 #Evaluar el desempeño del agente reflexivo frente al desempeño del agente aleatorio
 #Medida de desempeño y unidades de tiempo consumidas
 #Entornos 2x2 4x4 8x8 16x16 32x32 64x64 128x128
 #Porcentaje de suciedad en el ambiente 0.1,0.2,0.4,0.8
 #Repetir 10 veces cada combinacion
 #elaborar una tabla con los resultados en termino de la medida del rendimiento
-
-def run_simulation(env_size, dirt_rate, num_repeats):
-    random.seed(0)  # Fijamos la semilla para reproducibilidad
-    total_performance_random = 0
-    total_performance_reflexive = 0
-    total_time_random = 0
-    total_time_reflexive = 0
-
-    for _ in range(num_repeats):
-        env = Environment(sizeX=env_size, sizeY=env_size, init_posX=random.randint(0, env_size-1),
-                          init_posY=random.randint(0, env_size-1), dirt_rate=dirt_rate)
-        agent_random = AgentRandom(env)
-        agent_reflexive = AgentReflexive(env)
-
-        start_time = time.time()
-        while env.get_performance() < env_size * env_size:
-            action = agent_random.think()
-            env.accept_action(action)
-        end_time = time.time()
-        total_time_random += end_time - start_time
-        total_performance_random += env.get_performance()
-
-        env = Environment(sizeX=env_size, sizeY=env_size, init_posX=random.randint(0, env_size-1),
-                          init_posY=random.randint(0, env_size-1), dirt_rate=dirt_rate)
-        start_time = time.time()
-        while env.get_performance() < env_size * env_size:
-            action = agent_reflexive.think()
-            env.accept_action(action)
-        end_time = time.time()
-        total_time_reflexive += end_time - start_time
-        total_performance_reflexive += env.get_performance()
-
-    avg_performance_random = total_performance_random / num_repeats
-    avg_performance_reflexive = total_performance_reflexive / num_repeats
-    avg_time_random = total_time_random / num_repeats
-    avg_time_reflexive = total_time_reflexive / num_repeats
-
-    return avg_performance_random, avg_time_random, avg_performance_reflexive, avg_time_reflexive
-
-# Configuraciones de entorno y porcentaje de suciedad a evaluar
-env_sizes = [2, 4, 8, 16, 32, 64, 128]
-dirt_rates = [0.1, 0.2, 0.4, 0.8]
-num_repeats = 10
-
-# Crear una tabla con los resultados
-print("Entorno\tPorcentaje Suciedad\tAgente Aleatorio (Puntos)\tAgente Aleatorio (Tiempo)\tAgente Reflexivo (Puntos)\tAgente Reflexivo (Tiempo)")
-for env_size in env_sizes:
-    for dirt_rate in dirt_rates:
-        avg_perf_random, avg_time_random, avg_perf_reflexive, avg_time_reflexive = run_simulation(env_size, dirt_rate, num_repeats)
-        print(f"{env_size}x{env_size}\t{dirt_rate}\t{avg_perf_random:.2f}\t{avg_time_random:.2f}\t{avg_perf_reflexive:.2f}\t{avg_time_reflexive:.2f}")
